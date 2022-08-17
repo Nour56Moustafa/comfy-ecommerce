7dbf2f78b00cb0a4f647641d5507755f49d3c60d
Nour56Moustafa <110491958+Nour56Moustafa@users.noreply.github.com>
2022-08-17 22:14:47 +0300

    Update CartButton.js

--- src/component/CartButton.js
+++ src/component/CartButton.js
@@ -3,14 +3,16 @@ import { FaShoppingCart, FaUserMinus, FaUserPlus } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useCartContext } from "../context/cart_context";
 import { useUserContext } from "../context/user_context";
+import { useProductsContext } from "../context/products_context";
 
 const CartButton = () => {
   const { user, logout } = useUserContext();
-
   const { total_items, clearCart } = useCartContext();
+  const { closeSidebar } = useProductsContext();
+
   return (
     <div className="wrapper">
-      <Link to="/cart">
+      <Link to="/cart" onClick={closeSidebar}>
         Cart
         <span className="cart-container">
           <FaShoppingCart />
@@ -22,6 +24,7 @@ const CartButton = () => {
           type="button"
           className="auth-btn"
           onClick={() => {
+            closeSidebar();
             clearCart();
             logout();
           }}
@@ -29,7 +32,7 @@ const CartButton = () => {
           Logout <FaUserMinus />
         </button>
       ) : (
-        <Link className="auth-btn" to="login">
+        <Link className="auth-btn" to="login" onClick={closeSidebar}>
           Login <FaUserPlus />
         </Link>
       )}
